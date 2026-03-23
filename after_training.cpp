65ec49dbf43e62b8ca77701e26c1e8ebd7771365
Sanjida <sanjidasorme218@gmail.com>
2026-03-23 19:51:52 +0600

    prblm_4

---
+++ after_training.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/problemset/problem/195/B
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     long long n, m;
+     cin>>n>>m;
+     for(int i = 0; i<n; i++){
+        int k = i % m;
+        long long basket;
+        if((m+k)&1){
+            basket = (m+k+1)/2;
+        }else{
+            basket = (m-k)/2;
+        }
+        cout<<basket<<endl;
+     }
+}
